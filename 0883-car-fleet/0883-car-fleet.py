4cfcdaa50b5bd01f371dba7d7f9bdf15d45037e3
Shobhit Kulshreshtha <51631078+skulshreshtha@users.noreply.github.com>
2024-09-10 05:48:26 +0200

    Time: 656 ms (87.4%), Space: 34 MB (98.87%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.py
+++ 0883-car-fleet/0883-car-fleet.py
@@ -1,11 +1,11 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         fleet = []
-        cars = [[p, s] for p, s in zip(position, speed)]
-        
-        for p, s in sorted(cars, reverse=True):
-            fleet.append((target - p) / s)
-            if len(fleet) >= 2 and fleet[-1] <= fleet[-2]:
-                fleet.pop()
+        cars = [(p, s) for p, s in zip(position, speed)]
+
+        for c in sorted(cars, reverse=True):
+            time_current = (target - c[0])/c[1]
+            if not fleet or time_current > fleet[-1]:
+                fleet.append(time_current)
         
         return len(fleet)
