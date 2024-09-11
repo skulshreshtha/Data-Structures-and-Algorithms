4906bfe9a4c7da19aac039c944314a0a19ef431b
Shobhit Kulshreshtha <51631078+skulshreshtha@users.noreply.github.com>
2024-09-11 06:21:46 +0200

    Time: 88 ms (62.2%), Space: 16.7 MB (38.63%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,22 @@
+"""
+AABABBA
+l, maxLength, curLenght, counter, candidates
+0,1,1,{A:1},{A}
+0,2,2,{A:2},{A}
+0,3,3,{A:3},{A}
+0,
+"""
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l, maxLength = 0, 0
+        counter = collections.defaultdict(int)
+        for r in range(len(s)):
+            counter[s[r]] += 1
+            curLength = r - l + 1
+            # We need at least one character which has curLength-k occurences
+            if curLength - max(counter.values()) <= k:
+                maxLength = max(maxLength, curLength)
+            else:
+                counter[s[l]] -= 1
+                l += 1
+        return maxLength
